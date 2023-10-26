3e389905d394086bc34f9aa9284ee335d169b54f
Jose Hernandez <you@example.com>
2023-10-26 01:42:42 -0600

    react-todo

---
+++ src/pages/app.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Layout from "../templates/Layout";
+import Header from "../molecules/header";
+import AddTask from "../organisms/add-task";
+import TasksGroup from "../organisms/tasks-group";
+
+function App() {
+  const [tasks, setTasks] = React.useState([]);
+  const addTask = (taskName) => {
+    const isTaskNameInTask = tasks.includes( taskName.toLowerCase());
+    if (!isTaskNameInTask && taskName !== '' ) {
+      setTasks([...tasks, taskName.toLowerCase()]);
+    }
+  };
+  const deleteTask = (tasktoDelete) => {
+    const filteredTasksList = tasks.filter((task) => task !== tasktoDelete);
+    setTasks(filteredTasksList);
+  }
+  return (
+    <Layout>
+      <Header />
+      <AddTask onAddClick={addTask} />
+      <TasksGroup taskList={tasks} onDeleteTask={deleteTask} />
+    </Layout>
+  );
+}
+export default App;
